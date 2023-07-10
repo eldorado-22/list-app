851028ea97284e3dd46c1cc4ed405c42d5ec420b
eldorado-22 <jumashevv992@gmail.com>
2023-07-10 12:57:26 +0600

    .

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// let grocery = document.getElementByIdf('grocery')
+// grocery.addEventListener('submit', addItem);
+
+// function addItem(e) {
+//     e.preventDefault();
+//     let data = this.elements.writeList.value;
+//     let list = document.querySelector('ol')
+//     let item = document.createElement('li')
+//     let text = document.createElement('p')
+
+//     text.textContent = data;
+//     this.elements.writeList.value = "";
+//     item.append(text);
+//     list.append(item);
+     
+// }
